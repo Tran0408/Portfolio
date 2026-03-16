c83c8221d1ddba930764e619372f1ff07671cf65
Tran0408 <165976402+Tran0408@users.noreply.github.com>
2026-03-16 18:13:39 +0530

    Updated Download CV

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1OgP7xzVpRTZfoyWNRa8zgAIHZFAtJ2dM/view?usp=sharing"
+            href="https://drive.google.com/file/d/1f4t1dV6-H4W17n_3QR6tY18dKwyWuECC/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
